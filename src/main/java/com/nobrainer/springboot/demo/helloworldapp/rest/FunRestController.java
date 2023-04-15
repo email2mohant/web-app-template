dfb144c51206e77cef45450293c8e3c8ce5621b0
mohan <email2mohant@gmail.com>
2023-04-15 14:12:19 +0530

    + Info Endpoint is added

--- src/main/java/com/nobrainer/springboot/demo/helloworldapp/rest/FunRestController.java
+++ src/main/java/com/nobrainer/springboot/demo/helloworldapp/rest/FunRestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class FunRestController {
 
-    @Value("${info.app.name}")
+    @Value("${spring.app.name}")
     private String appName;
 
     @GetMapping("/")
